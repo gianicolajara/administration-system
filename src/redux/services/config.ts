5d653f17707e375e681f59def47e16e5841605c9
gianicola <gianicolajara@gmail.com>
2023-07-19 21:08:54 -0400

    new url api

--- src/redux/services/config.ts
+++ src/redux/services/config.ts
@@ -1,6 +1,4 @@
 export const urlApi =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000/api/"
-    : process.env.VERCEL === "1"
-    ? "https://administration-system-green.vercel.app/api/"
-    : "http://localhost:3000/api/";
+    : "https://administration-system-green.vercel.app/api/";
